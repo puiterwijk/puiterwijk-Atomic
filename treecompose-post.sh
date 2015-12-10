4138974acb171d8e1ae569db493070be4fc9d9f3
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-12-10 23:20:18 +0100

    Skip cleanup of languages
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- treecompose-post.sh
+++ treecompose-post.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # See: https://bugzilla.redhat.com/show_bug.cgi?id=1051816
-find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "en_US" -not -name "ja" -not -name "ja_JP" -exec rm -rf {} +
-localedef --list-archive | grep -a -v ^"en_US\|ja\|ja_jp" | xargs localedef --delete-from-archive
-mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
-build-locale-archive
+#find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "en_US" -not -name "ja" -not -name "ja_JP" -exec rm -rf {} +
+#localedef --list-archive | grep -a -v ^"en_US\|ja\|ja_jp" | xargs localedef --delete-from-archive
+#mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
+#build-locale-archive
