7559ca3919ac87f1e6e65d5dc7d20c3e345eeb10
Patrick Uiterwijk <puiterwijk@redhat.com>
2015-12-10 18:38:52 +0100

    Also retain ja(_JP) and make postscript executable
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- treecompose-post.sh
+++ treecompose-post.sh
@@ -3,8 +3,7 @@
 set -e
 
 # See: https://bugzilla.redhat.com/show_bug.cgi?id=1051816
-KEEPLANG=en_US
-find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "${KEEPLANG}" -exec rm -rf {} +
-localedef --list-archive | grep -a -v ^"${KEEPLANG}" | xargs localedef --delete-from-archive
+find /usr/share/locale -mindepth  1 -maxdepth 1 -type d -not -name "en_US" -not -name "ja" -not -name "ja_JP" -exec rm -rf {} +
+localedef --list-archive | grep -a -v ^"en_US\|ja\|ja_jp" | xargs localedef --delete-from-archive
 mv -f /usr/lib/locale/locale-archive /usr/lib/locale/locale-archive.tmpl
 build-locale-archive
