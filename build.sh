fee87b5577b01424823fa5fd0206337714df3334
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-01-19 17:52:51 +0100

    New volume and auto-formatting documentation
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -x
 export LANG=en_US.UTF-8
-export DATA_VOLID="vol-d0d7b123"
+export DATA_VOLID="vol-d90e452a"
 CONFIGDIR="/srv/rpm-ostree/config"
 exec >/root/script-setup.log 2>&1
 
