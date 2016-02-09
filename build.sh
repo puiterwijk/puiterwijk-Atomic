4a46507a84c4cc32573fd1a5dee682f77ca667f5
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-02-09 16:06:48 +0000

    Revert "no-clean"
    This reverts commit 0e504960ef707fb18b26a9396f3b47b0e0ac1015.

--- build.sh
+++ build.sh
@@ -125,27 +125,27 @@ mv /root/script-setup.log $LOGROOT/script-setup.log
 
 # Tear everything down again
 # Stop polipo
-#systemctl stop polipo.service
-#echo "Post-compose disk usage: "
-#df -h /dev/xvdf1
+systemctl stop polipo.service
+echo "Post-compose disk usage: "
+df -h /dev/xvdf1
 
 # Upload repo
-#if [ -f /srv/rpm-ostree/changed ];
-#jthen
- #   echo "Changed. Syncing"
- #j   echo "Changed" >$LOGROOT/changed
- #   rm -rf /mnt/data/repo/{tmp,uncompressed-objects-cache,state}
- #   aws s3 sync /mnt/data/repo s3://trees.puiterwijk.org/repo/ --acl public-read --delete
-#else
-#    echo "Not changed"
-#fi
+if [ -f /srv/rpm-ostree/changed ];
+then
+    echo "Changed. Syncing"
+    echo "Changed" >$LOGROOT/changed
+    rm -rf /mnt/data/repo/{tmp,uncompressed-objects-cache,state}
+    aws s3 sync /mnt/data/repo s3://trees.puiterwijk.org/repo/ --acl public-read --delete
+else
+    echo "Not changed"
+fi
 
 # Upload published info
-#aws s3 sync /srv/rpm-ostree/config/published s3://trees.puiterwijk.org/ --acl public-read
+aws s3 sync /srv/rpm-ostree/config/published s3://trees.puiterwijk.org/ --acl public-read
 
 # Sync data and write everything out
-#sync
-#umount /mnt/data
+sync
+umount /mnt/data
 
 # Self-termination
 #aws ec2 terminate-instances --instance-ids $AWS_INSTANCE_ID
