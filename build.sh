a0a006490cd41f5b71e80bef234c32256f5a9121
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-11-24 14:04:40 +0000

    New volume
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -x
 export LANG=en_US.UTF-8
-export DATA_VOLID="vol-7167e983"
+export DATA_VOLID="vol-007b117a8c8ec0a80"
 CONFIGDIR="/srv/rpm-ostree/config"
 exec >/root/script-setup.log 2>&1
 set -x
