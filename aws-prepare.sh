c5b535a8a61175188acce77c79f9628f2d896b9a
Patrick Uiterwijk <puiterwijk@redhat.com>
2016-12-30 00:17:33 +0000

    todos
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- aws-prepare.sh
+++ aws-prepare.sh
@@ -10,10 +10,10 @@ dnf update -y
 
 # Install required packages
 dnf install -y git rpm-ostree rpm-ostree-toolbox polipo docker fuse fuse-libs python-pip gnupg patch
-# Polipo isn't in F25+. For that, I need to look at squid. Sometime.
+# TODO: Polipo isn't in F25+. For that, I need to look at squid. Sometime.
 
 # Install pip
-pip install yas3fs
+pip install yas3fs  # TODO: This is now in Fedora repos. Good enough?
 pip install awscli
 
 # Apply patches
